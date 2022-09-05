cc0bc3f9d91f0c4abb2039b1322ce3f147911717
Roberto Sánchez <rsanchez@qualityobjects.com>
2022-09-05 18:23:17 +0200

    Update numbers with Hono 2.1.4 and Deno.serve

--- clone_and_run.sh
+++ clone_and_run.sh
@@ -13,11 +13,11 @@ git clone -b bun --single-branch https://github.com/rsc1975/h3lp3r-api.git h3lp3
 cd h3lp3r-bun && bun install
 cd ..
 git clone -b deno --single-branch https://github.com/rsc1975/h3lp3r-api.git h3lp3r-deno
-deno check h3lp3r-deno/src/index.ts
-
+deno check --unstable h3lp3r-deno/src/index.ts
 
 echo Deno test load init
 QUIET=true PORT=4004 deno run -A --unstable h3lp3r-deno/src/index.ts &
+sleep 1
 
 URL_BASE="http://localhost:4004" PLATFORM="deno" VERSION="unstable $(deno --version)" node run_test.mjs
 
@@ -30,6 +30,7 @@ sleep 10
 
 echo Bun test load init
 QUIET=true PORT=3003 bun run h3lp3r-bun/src/index.ts &
+sleep 1
 
 URL_BASE="http://localhost:3003" PLATFORM="bun" VERSION="$(bun --version)" node run_test.mjs
 
