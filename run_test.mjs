8e291addbd8f5a3122897eea2478e46b5c4e5608
Roberto Sánchez <rsanchez@qualityobjects.com>
2022-09-06 09:15:00 +0200

    Changed test tool to oha

--- run_test.mjs
+++ run_test.mjs
@@ -1,22 +1,26 @@
 
-import autocannon from 'autocannon';
+//import autocannon from 'autocannon';
 import fs from 'fs';
 import path from 'path';
 import os from 'os';
+import { exec } from 'child_process';
+import util from 'util';
+
+const execAsync = util.promisify(exec);
 
 if (!process.env.PLATFORM) {
   console.error('A "PLATFORM" var env should be specified');
   process.exit(-1);
-} 
+}
 
-const getHardware = () => {  
+const getHardware = () => {
   const cpus = os.cpus();
   const cores = cpus.length;
   const model = cpus[0].model;
   const arch = os.arch();
-  const mem = `${(os.totalmem() / (1024*1024*1024)).toFixed(1)} GB`;
+  const mem = `${(os.totalmem() / (1024 * 1024 * 1024)).toFixed(1)} GB`;
   const opsys = `${os.platform()} ${os.release()}`;
-  return {mem, arch, model, cores, os: opsys};
+  return { mem, arch, model, cores, os: opsys };
 }
 
 const savePlatformData = () => {
@@ -24,9 +28,9 @@ const savePlatformData = () => {
   let newData = {};
   const filepath = path.join('.', 'public', `platform.json`);
   try {
-    const existingPlatform = fs.readFileSync(filepath, { encoding: 'utf8'});
-    newData = JSON.parse(existingPlatform);    
-  } catch(e) {
+    const existingPlatform = fs.readFileSync(filepath, { encoding: 'utf8' });
+    newData = JSON.parse(existingPlatform);
+  } catch (e) {
     // Ignored
   }
   newData.hardware = hw;
@@ -36,7 +40,7 @@ const savePlatformData = () => {
 }
 
 const getCommands = () => {
-  const data = fs.readFileSync('./commands.json', {encoding: 'utf8'});
+  const data = fs.readFileSync('./commands.json', { encoding: 'utf8' });
   const cmds = JSON.parse(data);
   return cmds.order.filter(c => cmds.commands[c]).map(c => cmds.commands[c]);
 }
@@ -49,48 +53,53 @@ const saveChartFiles = (allChartsData) => {
     const filepath = path.join('public', `${chartName}.json`);
     let chartData = {};
     try {
-      const oldData = fs.readFileSync(filepath, {encoding: 'utf8'});
-      chartData = {...JSON.parse(oldData)};
-    } catch(e) {
-      const tpl = fs.readFileSync(path.join('tpl', 'chart.json'), {encoding: 'utf8'});
+      const oldData = fs.readFileSync(filepath, { encoding: 'utf8' });
+      chartData = { ...JSON.parse(oldData) };
+    } catch (e) {
+      const tpl = fs.readFileSync(path.join('tpl', 'chart.json'), { encoding: 'utf8' });
       const tplData = JSON.parse(tpl);
-      chartData = {...tplData};
+      chartData = { ...tplData };
     }
     chartData.labels = allChartsData.labels;
     const dataset = chartData.datasets.find((ds) => ds.label.toLowerCase() === platform);
     dataset.data = allChartsData[chartName];
-    
-    fs.writeFileSync(filepath, JSON.stringify(chartData, null, 2), {encoding: 'utf8'});
+
+    fs.writeFileSync(filepath, JSON.stringify(chartData, null, 2), { encoding: 'utf8' });
   }
-  
+
 }
-
-async function testCommand (urlBase, command) {
-  console.log(`Command: ${command.label}, wait for 5 secs...`)
-    const result = await autocannon({
-      title: command.label,
-      url: `${urlBase}${command.urlPath}`,
-      method: command.method || 'GET',
-      body: command.body && JSON.stringify(command.body),
-      connections: 200, //default
-      workers: 2,
-      duration: 5 // default
-    });
-    const summary = {
-      label: command.label,
-      url: result.url,
-      errors: result.errors,
-      latency: result.latency.average,
-      requests: result.requests.average,
-      throughput: (result.throughput.average / (1024*1024))
-    }
-
-    console.log(' - Errors:', summary.errors);
-    console.log(' - Latency (ms/req):', summary.latency);
-    console.log(' - Average (req/sec):', summary.requests);
-    console.log(' - Throughput (MB/sec):', summary.throughput.toFixed(2));
-    return summary;
+// oha -j -z 5s -c 200 'http://localhost:3003/api/base/64/encode?text=Lorem%20ipsum%20dolor%20sit%20amet&_pretty=1' --no-tui -m POST -d "{...}"
+async function testCommand(urlBase, command) {
+  console.log(`Command: ${command.label}, wait for 5 secs...`);
+  const body = command.body && JSON.stringify(command.body);
+  const bodyParam = body ? `-d '${body}'` : '';
+  const method = command.method || 'GET';
+  const url = `${urlBase}${command.urlPath}`;
+  const ohaParams = `-j -z 5s -c 200 --no-tui -m ${method} ${bodyParam}`;
+  const ohaCmd = `oha ${ohaParams} '${url}'`;
+  console.log('Executing:', ohaCmd)
+  const { stdout, stderr } = await execAsync(ohaCmd);
+  if (stderr) {
+    console.error(stderr);
+    throw new Error('Error calling: ' + stderr);
   }
+
+  const result = JSON.parse(stdout);
+  const summary = {
+    label: command.label,
+    url: url,
+    errors: 1.0 - result.summary.successRate,
+    latency: result.summary.average * 1000,
+    requests: result.summary.requestsPerSec,
+    throughput: (result.summary.sizePerSec / (1024 * 1024))
+  }
+
+  console.log(' - Errors:', summary.errors);
+  console.log(' - Latency (ms/req):', summary.latency.toFixed(2));
+  console.log(' - Average (req/sec):', summary.requests.toFixed(0));
+  console.log(' - Throughput (MB/sec):', summary.throughput.toFixed(2));
+  return summary;
+}
 
 // ------------------------------------------
 // |   Main execution:                      |
